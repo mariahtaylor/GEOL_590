7dc2fc6ba758013626f7e29ce3037fcdd40f8c8d
mariahtaylor <mariah.taylor1992@gmail.com>
2017-03-14 00:28:04 -0400

    Add files via upload

--- task5.R
+++ task5.R
@@ -1,3 +1,7 @@
+###############
+# We always start scripts with some comments about the script, like a title
+###############
+
 library(ggplot2)
 ggplot(data = mtcars)+
   geom_point(mapping = (aes(x= mpg, y= disp)))
