525ff7e6d33636f8696cb5d76d4005c65bfb23ef
mariahtaylor <mariah.taylor1992@gmail.com>
2017-03-14 00:31:53 -0400

    Delete task5.R

--- R/task5.R
+++
@@ -1,8 +0,0 @@
-###############
-# We always start scripts with some comments about the script, like a title
-###############
-
-library(ggplot2)
-ggplot(data = mtcars)+
-  geom_point(mapping = (aes(x= mpg, y= disp)))
-
