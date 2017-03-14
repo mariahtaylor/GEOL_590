4ad6cf8a2bec941b0b6c7e1f5310eb6676397162
mariahtaylor <mariah.taylor1992@gmail.com>
2017-03-14 14:38:49 -0400

    Update and rename task01_script.R to Task 2: task01_script.R

--- Task 2: task01_script.R
+++ Task 2: task01_script.R
@@ -1,16 +1,23 @@
+#Task 2
+
+#Add numbers and designate value "my.variable" to the sum of two numbers
 2+2
-a <- 2+2
-a*2
 my.variable <- 2+2
 my.variable/3
+print()
 
+#Make 3 vectors
 x <- seq(from=1, to=20, length.out=10)
 y <- rnorm(10)
 labels <- letters[10] 
 
 d <- data.frame(x, y, labels)
+
+#Save a plot
 png("test_plot.png", height=3, width=4, units="in", res=300)
 plot(x, y)
 dev.off()
+
+#Save a .csv file
 write.csv(d, "test_data.csv")
 
